63b37179af989cd7989b7ce78e7e5a51d7403a29
ElGatoPanzon <contact@elgatopanzon.io>
2026-05-08 15:50:45 -0600

    rendering: unified text draw for 2D and 3D with scale support

--- src/wm_raylib_rendering_handlers_draw.h
+++ src/wm_raylib_rendering_handlers_draw.h
@@ -55,25 +55,31 @@ static inline void wm_raylib_rendering_handle_draw_main_framebuffer(
 }
 
 
-static void DrawTextCodepoint3D(Font font, int codepoint, Vector3 position, float fontSize, bool backface, Color tint)
+static void DrawTextCodepointUniversial(Font font, int codepoint, Vector3 position, float fontSize, bool backface, Color tint)
 {
-    // Character index position in sprite font
-    // NOTE: In case a codepoint is not available in the font, index returned points to '?'
     int index = GetGlyphIndex(font, codepoint);
     float scale = fontSize/(float)font.baseSize;
 
-    // Character destination rectangle on screen
-    // NOTE: We consider charsPadding on drawing
-    position.x += (float)(font.glyphs[index].offsetX - font.glyphPadding)/(float)font.baseSize*scale;
-    position.z += (float)(font.glyphs[index].offsetY - font.glyphPadding)/(float)font.baseSize*scale;
-
-    // Character source rectangle from font texture atlas
-    // NOTE: We consider chars padding when drawing, it could be required for outline/glow shader effects
     Rectangle srcRec = { font.recs[index].x - (float)font.glyphPadding, font.recs[index].y - (float)font.glyphPadding,
                          font.recs[index].width + 2.0f*font.glyphPadding, font.recs[index].height + 2.0f*font.glyphPadding };
 
-    float width = (float)(font.recs[index].width + 2.0f*font.glyphPadding)/(float)font.baseSize*scale;
-    float height = (float)(font.recs[index].height + 2.0f*font.glyphPadding)/(float)font.baseSize*scale;
+    bool is3D = glIsEnabled(GL_DEPTH_TEST);
+
+    float width, height;
+    if (is3D)
+    {
+        position.x += (float)(font.glyphs[index].offsetX - font.glyphPadding)/(float)font.baseSize*scale;
+        position.z += (float)(font.glyphs[index].offsetY - font.glyphPadding)/(float)font.baseSize*scale;
+        width  = (float)(font.recs[index].width  + 2.0f*font.glyphPadding)/(float)font.baseSize*scale;
+        height = (float)(font.recs[index].height + 2.0f*font.glyphPadding)/(float)font.baseSize*scale;
+    }
+    else
+    {
+        position.x += (float)(font.glyphs[index].offsetX - font.glyphPadding)*scale;
+        position.y += (float)(font.glyphs[index].offsetY - font.glyphPadding)*scale;
+        width  = (float)(font.recs[index].width  + 2.0f*font.glyphPadding)*scale;
+        height = (float)(font.recs[index].height + 2.0f*font.glyphPadding)*scale;
+    }
 
     if (font.texture.id > 0)
     {
@@ -81,13 +87,12 @@ static void DrawTextCodepoint3D(Font font, int codepoint, Vector3 position, floa
         const float y = 0.0f;
         const float z = 0.0f;
 
-        // normalized texture coordinates of the glyph inside the font texture (0.0f -> 1.0f)
         const float tx = srcRec.x/font.texture.width;
         const float ty = srcRec.y/font.texture.height;
         const float tw = (srcRec.x+srcRec.width)/font.texture.width;
         const float th = (srcRec.y+srcRec.height)/font.texture.height;
 
-        rlCheckRenderBatchLimit(4 + 4*backface);
+        rlCheckRenderBatchLimit(4 + 4*(is3D && backface));
         rlSetTexture(font.texture.id);
 
         rlPushMatrix();
@@ -96,21 +101,32 @@ static void DrawTextCodepoint3D(Font font, int codepoint, Vector3 position, floa
             rlBegin(RL_QUADS);
                 rlColor4ub(tint.r, tint.g, tint.b, tint.a);
 
-                // Front Face
-                rlNormal3f(0.0f, 1.0f, 0.0f);                                   // Normal Pointing Up
-                rlTexCoord2f(tx, ty); rlVertex3f(x,         y, z);              // Top Left Of The Texture and Quad
-                rlTexCoord2f(tx, th); rlVertex3f(x,         y, z + height);     // Bottom Left Of The Texture and Quad
-                rlTexCoord2f(tw, th); rlVertex3f(x + width, y, z + height);     // Bottom Right Of The Texture and Quad
-                rlTexCoord2f(tw, ty); rlVertex3f(x + width, y, z);              // Top Right Of The Texture and Quad
-
-                if (backface)
+                if (is3D)
                 {
-                    // Back Face
-                    rlNormal3f(0.0f, -1.0f, 0.0f);                              // Normal Pointing Down
-                    rlTexCoord2f(tx, ty); rlVertex3f(x,         y, z);          // Top Right Of The Texture and Quad
-                    rlTexCoord2f(tw, ty); rlVertex3f(x + width, y, z);          // Top Left Of The Texture and Quad
-                    rlTexCoord2f(tw, th); rlVertex3f(x + width, y, z + height); // Bottom Left Of The Texture and Quad
-                    rlTexCoord2f(tx, th); rlVertex3f(x,         y, z + height); // Bottom Right Of The Texture and Quad
+                    // XZ plane: normal pointing up
+                    rlNormal3f(0.0f, 1.0f, 0.0f);
+                    rlTexCoord2f(tx, ty); rlVertex3f(x,         y, z);
+                    rlTexCoord2f(tx, th); rlVertex3f(x,         y, z + height);
+                    rlTexCoord2f(tw, th); rlVertex3f(x + width, y, z + height);
+                    rlTexCoord2f(tw, ty); rlVertex3f(x + width, y, z);
+
+                    if (backface)
+                    {
+                        rlNormal3f(0.0f, -1.0f, 0.0f);
+                        rlTexCoord2f(tx, ty); rlVertex3f(x,         y, z);
+                        rlTexCoord2f(tw, ty); rlVertex3f(x + width, y, z);
+                        rlTexCoord2f(tw, th); rlVertex3f(x + width, y, z + height);
+                        rlTexCoord2f(tx, th); rlVertex3f(x,         y, z + height);
+                    }
+                }
+                else
+                {
+                    // XY plane: normal toward camera (+Z)
+                    rlNormal3f(0.0f, 0.0f, 1.0f);
+                    rlTexCoord2f(tx, ty); rlVertex3f(x,         y,          0.0f);
+                    rlTexCoord2f(tx, th); rlVertex3f(x,         y + height, 0.0f);
+                    rlTexCoord2f(tw, th); rlVertex3f(x + width, y + height, 0.0f);
+                    rlTexCoord2f(tw, ty); rlVertex3f(x + width, y,          0.0f);
                 }
             rlEnd();
         rlPopMatrix();
@@ -119,19 +135,20 @@ static void DrawTextCodepoint3D(Font font, int codepoint, Vector3 position, floa
     }
 }
 
-// Draw a 2D text in 3D space
-static void DrawText3D(Font font, const char *text, Vector3 position, float fontSize, float fontSpacing, float lineSpacing, bool backface, Color tint)
+/* Draw text using unified 2D/3D path. Mode is detected once via GL_DEPTH_TEST
+   and drives coordinate layout and advance formula:
+   - 3D: XZ plane, world-unit advances (divided by baseSize)
+   - 2D: XY plane, pixel advances (scaled by fontSize/baseSize) */
+static void DrawTextUnified(Font font, const char *text, Vector3 position, float fontSize, float spacing, bool backface, Color tint)
 {
-    int length = TextLength(text);          // Total length in bytes of the text, scanned by codepoints in loop
-
-    float textOffsetY = 0.0f;               // Offset between lines (on line break '\n')
-    float textOffsetX = 0.0f;               // Offset X to next character to draw
-
+    int length = TextLength(text);
+    float textOffsetY = 0.0f;
+    float textOffsetX = 0.0f;
     float scale = fontSize/(float)font.baseSize;
+    bool is3D = glIsEnabled(GL_DEPTH_TEST);
 
     for (int i = 0; i < length;)
     {
-        // Get next codepoint from byte string and glyph index in font
         int codepointByteCount = 0;
         int codepoint = GetCodepoint(&text[i], &codepointByteCount);
         int index = GetGlyphIndex(font, codepoint);
@@ -142,79 +159,29 @@ static void DrawText3D(Font font, const char *text, Vector3 position, float font
 
         if (codepoint == '\n')
         {
-            // NOTE: Fixed line spacing of 1.5 line-height
-            // TODO: Support custom line spacing defined by user
-            textOffsetY += scale + lineSpacing/(float)font.baseSize*scale;
+            textOffsetY += is3D ? scale + 1.0f/(float)font.baseSize*scale : fontSize + 2;
             textOffsetX = 0.0f;
         }
         else
         {
             if ((codepoint != ' ') && (codepoint != '\t'))
             {
-                DrawTextCodepoint3D(font, codepoint, (Vector3){ position.x + textOffsetX, position.y, position.z + textOffsetY }, fontSize, backface, tint);
+                Vector3 pos = is3D
+                    ? (Vector3){ position.x + textOffsetX, position.y,             position.z + textOffsetY }
+                    : (Vector3){ position.x + textOffsetX, position.y + textOffsetY, 0.0f };
+                DrawTextCodepointUniversial(font, codepoint, pos, fontSize, backface, tint);
             }
 
-            if (font.glyphs[index].advanceX == 0) textOffsetX += (float)(font.recs[index].width + fontSpacing)/(float)font.baseSize*scale;
-            else textOffsetX += (float)(font.glyphs[index].advanceX + fontSpacing)/(float)font.baseSize*scale;
+            if (font.glyphs[index].advanceX == 0)
+                textOffsetX += is3D ? (float)(font.recs[index].width  + spacing)/(float)font.baseSize*scale
+                                    : (float) font.recs[index].width  *scale + spacing;
+            else
+                textOffsetX += is3D ? (float)(font.glyphs[index].advanceX + spacing)/(float)font.baseSize*scale
+                                    : (float) font.glyphs[index].advanceX  *scale + spacing;
         }
 
-        i += codepointByteCount;   // Move text bytes counter to next codepoint
+        i += codepointByteCount;
     }
-}
-
-// Measure a text in 3D. For some reason `MeasureTextEx()` just doesn't seem to work so i had to use this instead.
-static Vector3 MeasureText3D(Font font, const char* text, float fontSize, float fontSpacing, float lineSpacing)
-{
-    int len = TextLength(text);
-    int tempLen = 0;                // Used to count longer text line num chars
-    int lenCounter = 0;
-
-    float tempTextWidth = 0.0f;     // Used to count longer text line width
-
-    float scale = fontSize/(float)font.baseSize;
-    float textHeight = scale;
-    float textWidth = 0.0f;
-
-    int letter = 0;                 // Current character
-    int index = 0;                  // Index position in sprite font
-
-    for (int i = 0; i < len; i++)
-    {
-        lenCounter++;
-
-        int next = 0;
-        letter = GetCodepoint(&text[i], &next);
-        index = GetGlyphIndex(font, letter);
-
-        // NOTE: normally we exit the decoding sequence as soon as a bad byte is found (and return 0x3f)
-        // but we need to draw all of the bad bytes using the '?' symbol so to not skip any we set next = 1
-        if (letter == 0x3f) next = 1;
-        i += next - 1;
-
-        if (letter != '\n')
-        {
-            if (font.glyphs[index].advanceX != 0) textWidth += (font.glyphs[index].advanceX+fontSpacing)/(float)font.baseSize*scale;
-            else textWidth += (font.recs[index].width + font.glyphs[index].offsetX)/(float)font.baseSize*scale;
-        }
-        else
-        {
-            if (tempTextWidth < textWidth) tempTextWidth = textWidth;
-            lenCounter = 0;
-            textWidth = 0.0f;
-            textHeight += scale + lineSpacing/(float)font.baseSize*scale;
-        }
-
-        if (tempLen < lenCounter) tempLen = lenCounter;
-    }
-
-    if (tempTextWidth < textWidth) tempTextWidth = textWidth;
-
-    Vector3 vec = { 0 };
-    vec.x = tempTextWidth + (float)((tempLen - 1)*fontSpacing/(float)font.baseSize*scale); // Adds chars spacing to measure
-    vec.y = 0.25f;
-    vec.z = textHeight;
-
-    return vec;
 }
 
 
@@ -231,40 +198,52 @@ static inline void wm_raylib_rendering_handle_draw_text(
 
 	if (GetFontDefault().texture.id != 0)
 	{
-		/* int defaultFontSize = 10;   // default Font chars height in pixel */
-		/* if (cmd->font_size < defaultFontSize) cmd->font_size = defaultFontSize; */
-		/* int spacing = cmd->font_size/defaultFontSize; */
-
-		// extract Y rotation angle from quat for 2D spin
-		float yaw = w_quat_to_euler(cmd->rotation).y;
 		rlPushMatrix();
 
-			if (glIsEnabled(GL_DEPTH_TEST))
-			{
-				w_mat4 rs = w_mat4_from_trs(
-					((w_vec3){0, 0, 0}),  // no position in matrix
-					cmd->rotation,
-					((w_vec3){1, 1, 1})
-				);
+		bool is3D = glIsEnabled(GL_DEPTH_TEST);
 
-				rlTranslatef(cmd->position.x, cmd->position.y, cmd->position.z);
-				rlMultMatrixf(rs.m);
-				rlTranslatef(-cmd->origin.x, -cmd->origin.y, -cmd->origin.z);
+		// if not 3D, re-write for raylibs odd screen mode camera
+		// note: this is mostly to keep the incoming XZ and -Z convention for
+		// both world (camera) and overlay (screen) phases
+		if (!is3D)
+		{
+			// rotate +90 degrees on X to negate the incoming -90 degrees on X
+			cmd->rotation = w_quat_mul(cmd->rotation, w_quat_rotation_x_deg(90.0f));
 
-				// get world size from pixel scale
-				float base_size = (float)GetFontDefault().baseSize;
-				float world_font_size = ((float)cmd->font_size * base_size) / W_RENDERING_WORLD_PIXEL_SCALE;
+			// swap Z and Y for position and scale and origin
+			float old_pos_y = cmd->position.y;
+			cmd->position.y = cmd->position.z;
+			cmd->position.z = old_pos_y;
 
-				DrawText3D(GetFontDefault(), cmd->text, ((Vector3){0, 0, 0}), world_font_size, cmd->font_spacing, 1.0f, true, w2rl_color(cmd->font_color));
-			}
-			else
-			{
-    			rlTranslatef(cmd->position.x, cmd->position.z, 0.0f);
-    			rlRotatef(yaw * RAD2DEG, 0, 0, 1);  // Z rotation in screen space
-    			rlTranslatef(-cmd->origin.x, -cmd->origin.z, 0.0f);
+			float old_scale_y = cmd->scale.y;
+			cmd->scale.y = cmd->scale.z;
+			cmd->scale.z = old_scale_y;
 
-				DrawTextEx(GetFontDefault(), cmd->text, ((Vector2){ 0.0f, 0.0f }), cmd->font_size, cmd->font_spacing, w2rl_color(cmd->font_color));
-			}
+			float old_origin_y = cmd->origin.y;
+			cmd->origin.y = cmd->origin.z;
+			cmd->origin.z = old_origin_y;
+		}
+
+		// build matrix without position
+		w_mat4 rs = w_mat4_from_trs(
+			((w_vec3){0, 0, 0}),
+			cmd->rotation,
+			cmd->scale
+		);
+
+		rlTranslatef(cmd->position.x, cmd->position.y, cmd->position.z);
+		rlMultMatrixf(rs.m);
+		rlTranslatef(-cmd->origin.x, -cmd->origin.y, -cmd->origin.z);
+
+		// set font size to world pixel scale
+		if (is3D)
+		{
+			float base_size = (float)GetFontDefault().baseSize;
+			cmd->font_size = ((float)cmd->font_size * base_size) / W_RENDERING_WORLD_PIXEL_SCALE;
+		}
+
+		// draw the text
+		DrawTextUnified(GetFontDefault(), cmd->text, ((Vector3){ 0.0f, 0.0f, 0.0f }), cmd->font_size, cmd->font_spacing, true, w2rl_color(cmd->font_color));
 
 		rlPopMatrix();
 	}
